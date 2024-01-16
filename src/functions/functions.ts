8295a5ddbcd8498380cbbe485ae59e920d1cf69a
Robin Bezak <bezakrobin@gmail.com>
2024-01-16 05:53:37 +0100

    added arrow functions

---
+++ src/functions/functions.ts
@@ -0,0 +1,31 @@
+export const handleArrowUpMouseUp = () => {
+    console.log('ARROW UP mouse up');
+};
+
+export const handleArrowDownMouseUp = () => {
+    console.log('ARROW DOWN mouse up');
+};
+
+export const handleArrowLeftMouseUp = () => {
+    console.log('ARROW LEFT mouse up');
+};
+
+export const handleArrowRightMouseUp = () => {
+    console.log('ARROW RIGHT mouse up');
+};
+
+export const handleArrowUpKeyUp = () => {
+    console.log('ARROW UP key up');
+};
+
+export const handleArrowDownKeyUp = () => {
+    console.log('ARROW DOWN key up');
+};
+
+export const handleArrowLeftKeyUp = () => {
+    console.log('ARROW LEFT key up');
+};
+
+export const handleArrowRightKeyUp = () => {
+    console.log('ARROW RIGHT key up');
+};
