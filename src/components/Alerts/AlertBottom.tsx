9997772343ee70f6bdec1477469cd5a059596978
Robin Bezak <bezakrobin@gmail.com>
2024-01-17 05:10:14 +0100

    added hint bottom, created alert bottom

---
+++ src/components/Alerts/AlertBottom.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './AlertStyle.css';
+
+export const AlertBottom: React.FC = () => {
+    return (
+        <div className="alert-bottom">
+        </div>
+    );
+};
--- src/utils/pageRenderFunctions.tsx
+++ src/utils/pageRenderFunctions.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import {Menu} from "../components/Menu/Menu";
 import {PageTitle} from "../components/PageTitle/PageTitle";
 import {Spacing} from "../components/Spacing/Spacing";
+import {HintBottom} from "../components/Hints/HintBottom";
 
 export const renderHomePage = (): React.ReactNode => (
     <>
         <Spacing key="spacing" direction={"vertical"} spacing={10}/>
         <PageTitle key="pageTitle" text={'Robin Bezak'}/>
         <Menu key="menu" />
+        <HintBottom />
     </>
 );
