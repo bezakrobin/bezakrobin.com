9c40eee144b899be6b8f7d9044f7acc9dd9c0d92
Robin Bezak <bezakrobin@gmail.com>
2024-01-17 13:42:31 +0100

    added keyboard to legend, added logic, added highlight css for testing for now

--- src/components/Overlays/Legend/Legend.tsx
+++ src/components/Overlays/Legend/Legend.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './LegendStyle.css';
+import KeyboardLayoutSVG from '../../../images/Keyboard.svg';
 
 export const Legend: React.FC = () => {
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            const keyElement = document.getElementById(`key-${event.key.toLowerCase()}`);
+            if (keyElement) {
+                keyElement.classList.add('highlight');
+            }
+        };
+
+        const handleKeyUp = (event: KeyboardEvent) => {
+            const keyElement = document.getElementById(`key-${event.key.toLowerCase()}`);
+            if (keyElement) {
+                keyElement.classList.remove('highlight');
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+        window.addEventListener('keyup', handleKeyUp);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+            window.removeEventListener('keyup', handleKeyUp);
+        };
+    }, []);
+
     return (
         <div className="legend">
-
+            <img src={KeyboardLayoutSVG} alt="Keyboard Layout" />
         </div>
     );
 };
