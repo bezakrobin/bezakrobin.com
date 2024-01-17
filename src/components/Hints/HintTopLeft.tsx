4ce7fa9b397778e1ca277eca63ce173fc00fcdd8
Robin Bezak <bezakrobin@gmail.com>
2024-01-17 06:40:24 +0100

    added key up func to toggle legend overlay

--- src/components/Hints/HintTopLeft.tsx
+++ src/components/Hints/HintTopLeft.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import EscapeKeyImg from '../../images/Escape.svg';
 import './HintStyle.css';
 import { NavigationContext } from '../../contexts/NavigationContext';
@@ -6,6 +6,18 @@ import { toggleShowLegend } from '../../utils/functions';
 
 export const HintTopLeft: React.FC = () => {
     const { setShowLegend, showLegend } = useContext(NavigationContext);
+
+    useEffect(() => {
+        const handleKeyUp = (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                toggleShowLegend(setShowLegend, showLegend);
+            }
+        };
+
+        window.addEventListener('keyup', handleKeyUp);
+
+        return () => window.removeEventListener('keyup', handleKeyUp);
+    }, [setShowLegend, showLegend]);
 
     return (
         <div className="hint-top-left">
